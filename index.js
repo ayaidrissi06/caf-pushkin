9ee1e98164411af5a0de2074b19e797d0f2a010c
ayaidrissi06 <aya18idrissi@gmail.com>
2026-04-14 11:47:41 +0100

    café

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-// On récupère le bouton (le toggle) et la liste des liens (la navbar)
 const toogle = document.getElementById("toogle");
-const navbar = document.getElementById("navbar");
+const navbar = document.querySelector(".nav-links"); // On cible la liste de liens
 
-// On vérifie dans la console si les éléments sont bien trouvés
-console.log("Bouton trouvé :", toogle);
-console.log("Menu trouvé :", navbar);
-
-// Au clic, on ajoute ou on enlève la classe "apparait"
 toogle.addEventListener("click", () => {
     navbar.classList.toggle("apparait");
 });
